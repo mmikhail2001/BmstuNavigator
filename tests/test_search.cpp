36f1541e181221aa7278acaf4d0425a4aa1980cc
Artyom1363 <artemwulf2015@gmail.com>
2022-05-27 20:31:41 +0000

    added fix to not exist id

--- tests/test_search.cpp
+++ tests/test_search.cpp
@@ -204,6 +204,15 @@ TEST(TestRoute, test_399u_403u) {
     EXPECT_EQ(foundRoute[3].linkToFile, "3->13.txt");
 }
 
+TEST(TestRoute, test_1u) {
+    DataBaseTest data;
+    Search s1(&data);
+    vector <Point*> from = s1.GetByName("399u");
+    unsigned int id = from[0]->GetId();
+    optional <Route> optRoute = s1.FindRoute(1111111, "403u");
+    EXPECT_FALSE(optRoute);
+}
+
 TEST(TestRoute, test_401u_403u) {
     DataBaseTest data;
     Search s1(&data);
